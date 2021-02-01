298d231c12eb7f78380afeeae4cb0c4775a6097e
Ricardo Magalhães <ricardomrfin@gmail.com>
2021-02-01 08:51:40 -0300

    Put e delete funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
@@ -11,6 +13,8 @@ const routes: Routes = [
   {path:'entrar',component: EntrarComponent},
   {path:'cadastrar',component:CadastrarComponent}, 
   {path:'tema',component:TemaComponent}, 
+  {path:'tema-edit/:id',component:TemaEditComponent}, 
+  {path:'tema-delete/:id',component:TemaDeleteComponent}, 
   
 
   {path:'inicio',component:InicioComponent}
